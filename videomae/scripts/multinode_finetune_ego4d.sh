1e5ec8eff2ef3e01f85f7eb8207b69646fdfbacf
jiachen9942 <893470334@qq.com>
2022-07-17 01:13:53 +0800

    debug

--- videomae/scripts/multinode_finetune_ego4d.sh
+++ videomae/scripts/multinode_finetune_ego4d.sh
@@ -6,7 +6,7 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch \
     --enable_deepspeed \
     --dist_eval \
     --overwrite command-line \
-    --config /mnt/code/videomae/config/finetune_basic_ego4d.yml \
+    --config /mnt/code/videomae/config/local/finetune_basic_ego4d.yml \
     --debug \
     --name prek400ftego4d_A1
  
