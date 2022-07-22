33ad6d7c2b88585c264f7ee7ef113ea3badcc368
jiachen9942 <893470334@qq.com>
2022-07-22 19:58:22 +0800

    add tar file cache mechanism

--- videomae/test_cwd.py
+++ videomae/test_cwd.py
@@ -1,5 +1,5 @@
 from epickitchens_utils import extract_zip
 
-path_to_save="/data/shared/ssvl/epic-kitchens55/3h91syskeag572hl6tvuovwv4d/frames_rgb_flow/flow/test/P01/P01_13"
+path_to_save="/data/shared/ssvl/epic-kitchens50/3h91syskeag572hl6tvuovwv4d/frames_rgb_flow/flow/test/P01/P01_13"
 
 extract_zip(path_to_save, ext="tar", frame_list = ["frame_{:010d}.jpg".format(i) for i in range(1, 5)], flow=True)
