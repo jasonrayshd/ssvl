5af707a0a47ddeb332251125766272c07ca1d87d
jiachen_temp <893470334@qq.com>
2022-08-06 21:30:29 +0800

    jiaxing

--- videomae/scripts/local_finetune_ego4d_config.sh
+++ videomae/scripts/local_finetune_ego4d_config.sh
@@ -1,15 +1,16 @@
 
 OMP_NUM_THREADS=1 python -m torch.distributed.launch \
-    --nproc_per_node=4 \
-    --master_port 51225 --nnodes=1  --node_rank=$1 --master_addr=$2 \
+    --nproc_per_node=2 \
+    --master_port 51225 --nnodes=2  --node_rank=$1 --master_addr=$2 \
     ../run_class_finetuning.py \
     --enable_deepspeed \
     --dist_eval \
     --overwrite command-line \
-    --config /mnt/code/ssvl/videomae/config/local/finetune_basic_ego4d.yml \
-    --project pt-epic55-ft-ego4dsc \
-    --name preepic55ftego4d_A0 \
-    --wandb_id 3ccex8ya \
-    # --project temp \
-    # --name temp \
-    # --debug \
+    --config /data/shared/ssvl/videomae/config/local/finetune_basic_ego4d.yml \
+    --project temp \
+    --name temp \
+    --debug \
+
+    # --project pt-epic55-ft-ego4dsc \
+    # --name preepic55ftego4d_A0 \
+    # --wandb_id 3ccex8ya \
--- videomae/scripts/local_pretrain_ts_epic.sh
+++ videomae/scripts/local_pretrain_ts_epic.sh
@@ -1,8 +1,13 @@
 
-OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=4 \
-        --master_port 12320 --nnodes=1 --node_rank=$1 --master_addr=$2 \
+OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=2 \
+        --master_port 12320 --nnodes=2 --node_rank=$1 --master_addr=$2 \
         ../run_mae_pretraining.py \
         --overwrite command-line \
-        --config /mnt/code/ssvl/videomae/config/local/pretrain_ts_epic55.yml \
+        --config /data/shared/ssvl/videomae/config/local/pretrain_ts_epic55.yml \
         --project pretrain_ts_epic55 \
-        --name ts_preepic55_A2 \
+        --name ts_preepic55_A3 \
+
+        # --project temp \
+        # --name temp \
+        # --debug \
+   
