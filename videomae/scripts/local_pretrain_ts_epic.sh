cdb70094135ecde3e5568c994160718577c9eef2
jiachen_temp <893470334@qq.com>
2022-08-10 20:33:34 +0800

    jiaxing

--- videomae/scripts/local_pretrain_ts_epic.sh
+++ videomae/scripts/local_pretrain_ts_epic.sh
@@ -1,9 +1,8 @@
-
-OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=2 \
+OMP_NUM_THREADS=40 python -m torch.distributed.launch --nproc_per_node=2 \
         --master_port 12320 --nnodes=2 --node_rank=$1 --master_addr=$2 \
         ../run_mae_pretraining.py \
         --overwrite command-line \
-        --config /data/shared/ssvl/videomae/config/local/pretrain_ts_epic55.yml \
+        --config /data/shared/ssvl/videomae/config/temp/pretrain_ts_epic55.yml \
         --project pretrain_ts_epic55 \
-        --name ts_preepic55_A9 \
+        --name ts_preepic55_A10 \
         # --debug \
--- videomae/scripts/local_test_ego4d.sh
+++ videomae/scripts/local_test_ego4d.sh
@@ -1,9 +1,9 @@
 OMP_NUM_THREADS=1 python -m torch.distributed.launch \
-    --nproc_per_node=2 \
-    --master_port 51225 --nnodes=2  --node_rank=$1 --master_addr=$2 \
+    --nproc_per_node=1 \
+    --master_port 51225 --nnodes=1  --node_rank=$1 --master_addr=$2 \
     ../test_on_ego4d.py \
-    --dist_eval \
     --overwrite command-line \
-    --name temp \
+    --name preepic55ftego4d_A2 \
     --config /data/shared/ssvl/videomae/config/temp/test_basic_ego4d.yml \
-    --ckpt /data/shared/output/preepic55ftego4d_A0/checkpoint-99/mp_rank_00_model_states.pt
+    --ckpt /data/shared/output/preepic55ftego4d_A2/mp_rank_00_model_states.pt
+    # --dist_eval \
