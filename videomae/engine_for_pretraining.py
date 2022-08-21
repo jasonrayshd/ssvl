c1f2024df81968023532936985a659d3a87280ea
jiachen9942 <893470334@qq.com>
2022-08-22 00:33:24 +0800

    debug

--- videomae/engine_for_pretraining.py
+++ videomae/engine_for_pretraining.py
@@ -519,7 +519,7 @@ def train_tsvit_one_epoch(model: torch.nn.Module, data_loader: Iterable, optimiz
             # weight[:,:,0,...] = (weight[:,:, 0,...] - weight[:,:, 0, ...].mean())
             # weight[:,:,1,...] = (weight[:,:, 1,...] - weight[:,:, 1, ...].mean())
 
-            weight -= 0.5
+            weight = flows - 0.5
             weight = torch.sqrt(weight[:, 0, ...]**2 + weight[:, 1, ...]**2) # B, T=8, H, W
             B, T, H, W = weight.shape
             weight = F.normalize(weight.flatten(2), p=2, dim=2).reshape(B, T, H, W)
