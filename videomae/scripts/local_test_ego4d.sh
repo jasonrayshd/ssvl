3ca312a85c005d0b426ba025a2399ba134459b4a
jiachen9942 <893470334@qq.com>
2022-08-23 11:01:29 +0800

    exp

--- videomae/scripts/local_test_ego4d.sh
+++ videomae/scripts/local_test_ego4d.sh
@@ -3,7 +3,7 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch \
     --master_port 51225 --nnodes=1  --node_rank=$1 --master_addr=$2 \
     ../test_on_ego4d.py \
     --overwrite command-line \
-    --name preepic55ftego4d_A4 \
+    --name preepic55ftego4d_A3 \
     --config /mnt/code/ssvl/videomae/config/local/test_basic_ego4d.yml \
-    --ckpt /data/jiachen/ssvl_output/preepic55ftego4d_A4/mp_rank_00_model_states.pt \
+    --ckpt /data/jiachen/ssvl_output/preepic55ftego4d_A3/mp_rank_00_model_states.pt \
     --dist_eval \
