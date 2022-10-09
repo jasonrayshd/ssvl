5044c906d1527eea705dee88b66b65733f2d6341
jiachen9942 <893470334@qq.com>
2022-10-09 22:36:42 +0800

    add flow extraction code of egoclip

--- videomae/toolbox/extract_flow.sh
+++ videomae/toolbox/extract_flow.sh
@@ -12,15 +12,15 @@ NEW_ANNOTATION_FILE="/home/leijiachen/ssvl/videomae/new_egoclip.csv"
 # maximum number of threads on each gpu to extract flow
 # e.g. if the cuda memory of gpu is 32G then 64 is recommended
 #                   ...             24G then 32 is recommended
-MAX_NUM_THREADS=32
+MAX_NUM_THREADS=2
 
 # available gpu ids
 # $MAX_NUM_THREADS*${#GPUS[@]} number of threads will be started
-GPUS=(0 1 5 3)
+GPUS=(3 4)
 
 # total threads
 thread_num=$(( ${#GPUS[@]}*$MAX_NUM_THREADS ))
-
+echo number of threads: $thread_num
 
 function run_docker(){
 
@@ -28,77 +28,93 @@ function run_docker(){
     local source=$2
     local logfile=$3
 
-    
     local gpu=$(echo $gpu_clip_id | cut -d ":" -f 1)
     local clip_id=$(echo $gpu_clip_id | cut -d ":" -f 2)
     local uid=$(echo $clip_id | cut -d "_" -f 1)
 
     local in="$source/$uid/$clip_id/rgb"
+    local zjuin="$source/$uid/$clip_id"
     local out="$source/$uid/$clip_id/flow"
 
-    # mkdir if not exist and change directory permission
+    # for local server
+    # mkdir if $in not exist and change directory permission
+    [ -d $in ] || mkdir -p $in
+    local localf=($(ls $zjuin | grep jpeg))
+    if [[ ${#localf[@]} -ne 0 ]];then
+        for f in ${localf[@]};do
+            mv "$zjuin/$f" "$in"
+        done
+    fi
+
+    # mkdir if $out not exist and change directory permission
     [ -d $out ] || mkdir $out
+
     chmod 777 $in
     chmod 777 $out
 
-    # echo $gpu $clip_id $in $out done
-
+    # rename image file to %05d.jpg and start from 00000.jpg
     rgb_frames=$(ls $in | sort)
-    echo ${rgb_frames[@]}
+    # echo ${rgb_frames[@]}
 
-    # rename
-    # num=0
-    # for frame in ${rgb_frames[@]};do
-    #     new_name=$(printf "%05d.jpg" $num)
-    #     mv "$in/$frame" "$in/$new_name"
-    #     num=$((num+1))
-    #     # echo $new_name, $num
-    # done
+    if [[ ${#rgb_frames[@]} -eq 0 ]];then
+        echo "$clip_id,fail" >> $logfile
+        return 1
+    fi
 
+    num=0
+    for frame in ${rgb_frames[@]};do
+        new_name=$(printf "%05d.jpg" $num)
+        if [[ "$in/$frame" == "$in/$new_name" ]];then
+            continue
+        fi
+        mv "$in/$frame" "$in/$new_name"
+        num=$((num+1))
+        # echo $new_name, $num
+    done
 
-    # docker run --gpus "device=$gpu" \
-    #         --rm \
-    #         -v "$in:/input" \
-    #         -v "$out:/output" \
-    #         willprice/furnari-flow \
-    #         %05d.jpeg -g 0 -s 1 -d 1 -b 8 > /dev/null 2>&1
+    docker run --gpus "device=$gpu" \
+            --rm \
+            -v "$in:/input" \
+            -v "$out:/output" \
+            willprice/furnari-flow \
+            %05d.jpg -g 0 -s 1 -d 1 -b 8 > /dev/null 2>&1
 
-    # if [[ $? -ne 0  ]];then
-    #     echo "$clip_id,fail" >> $logfile
-    # else
-    #     echo "$clip_id,success" >> $logfile
-    # fi
+    if [[ $? -ne 0  ]];then
+        echo "$clip_id,fail" >> $logfile
+    else
+        echo "$clip_id,success" >> $logfile
+    fi
 
 }
 
 export -f run_docker
 
-# echo "Reading $NEW_ANNOTATION_FILE ..."
-# clips=()
-# idx=0
-# num=0
-# while IFS= read -r line; do
-#     fields=($(echo $line | rev | cut -d " " -f 2-4 | rev))
-#     clip_id=${fields[0]}
-#     clips+=("${GPUS[$idx]}:$clip_id")
+# determine clips to be processed
+echo "Preparing information of clips to be processed..."
+py_ret=($(/opt/anaconda3/bin/python filter_processed_flow.py --logfile $LOGFILE --anno_path $NEW_ANNOTATION_FILE))
 
-#     # echo ${GPUS[$idx]}, $idx    
+processed_num="${py_ret[0]}"
+clips_raw=("${py_ret[@]:1}")
+echo "Done. $processed_num clips have been processed. A total of ${#clips_raw[@]} clips will be processed"
 
-#     idx=$((idx+1))
-#     idx=$((idx % ${#GPUS[@]}))
+# determine gpu id for each clip
+clips=()
+idx=0
+num=0
+for clip in ${clips_raw[@]};do
 
-#     below codes are for debugging
-#     num=$((num+1))
-#     if [[ $num -eq 10 ]];then
-#         break
-#     fi
+    # echo ${GPUS[$idx]}, $idx    
+    clips+=("${GPUS[$idx]}:$clip")
+    idx=$((idx+1))
+    idx=$((idx % ${#GPUS[@]}))
 
-# done < "$NEW_ANNOTATION_FILE"
+    # below codes are for debugging
+    # num=$((num+1))
+    # if [[ $num -eq 10 ]];then
+    #     break
+    # fi
+done
 
+echo "Start extracting flows... Please check $LOGFILE for current progress"
 
-echo "Start extracting flows"
-
-# echo ${clips[@]} | xargs -n 1 -P $thread_num -i bash -c "run_docker {} $SOURCE $LOGFILE"
-
-# for debugging
-echo 0:865733f5-97b6-4380-a418-1fd6510e0f5e_00001 | xargs -n 1 -i bash -c "run_docker {} $SOURCE $LOGFILE" done
+printf "%s\n" "${clips[@]}" | xargs -n 1 -P $thread_num -i bash -c "run_docker {} $SOURCE $LOGFILE;echo \r processing..." done
